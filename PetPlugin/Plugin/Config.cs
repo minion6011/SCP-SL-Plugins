f621e2b631211f455e7a9dc4f0776a38fdc4a8b3
Minion6011 <mvittorio29@gmail.com>
2025-06-24 16:37:39 +0200

    Add files via upload

---
+++ PetPlugin/Plugin/Config.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace PetPlugin;
+
+public class Config
+{
+    public Dictionary<string, List<string>> PetLists { get; set; } = new Dictionary<string, List<string>> {
+        ["Vip"] = new List<string> { "Ghost", "SCP173", "Mimic", "Toilet", "Amongus"},
+        ["Booster"] = new List<string> { "Booster" },
+    };
+    public Dictionary<string, List<string>> UserList { get; set; } = new Dictionary<string, List<string>>
+    {
+        ["Vip"] = new List<string> { "1234@steam" },
+        ["Booster"] = new List<string> { "1234@steam" },
+    };
+
+}
---
+++ PetPlugin/Plugin/PetManager.cs
@@ -0,0 +1,53 @@
+﻿using LabApi.Features.Wrappers;
+using MEC;
+using ProjectMER.Features;
+using ProjectMER.Features.Objects;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PetPlugin;
+
+public class PetManager
+{
+    public static Dictionary<Player, SchematicObject> SchematicsPets { get; private set; } = new Dictionary<Player, SchematicObject>();
+    private static bool CoroutineRunning { get; set; } = false;
+    public static void SpawnPet(Player player, string petname)
+    {
+        if (SchematicsPets.ContainsKey(player)) {
+            RemovePet(player);
+        }
+        SchematicsPets.Add(player, ObjectSpawner.SpawnSchematic(petname, player.Position + new Vector3(-0.3f, 0.3f, 0.5f), player.Rotation, Vector3.one));
+
+        if (!CoroutineRunning)
+        {
+            Timing.RunCoroutine(PositionCoroutine(), "PetPositionUpdate");
+            CoroutineRunning = true;
+        }
+    }
+    public static void RemovePet(Player player)
+    {
+        if (SchematicsPets.ContainsKey(player))
+        {
+            SchematicsPets[player].Destroy();
+            SchematicsPets.Remove(player);
+        }
+    }
+
+    internal static IEnumerator<float> PositionCoroutine()
+    {
+        while (true)
+        {
+            foreach (var el in SchematicsPets) {
+
+                Vector3 forward = el.Key.Rotation * Vector3.forward;
+                Vector3 right = el.Key.Rotation * Vector3.right;
+                el.Value.Position = el.Key.Position + (right * 0.6f) + (-forward * 0.25f)  + new Vector3(0, 0.3f, 0);
+
+                el.Value.Rotation = el.Key.Rotation;
+            }
+
+            yield return Timing.WaitForSeconds(0.015f);
+        }
+    }
+
+}
