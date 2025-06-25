88c87339e00ea0ccfd006880c80ca2156f28dea7
Minion6011 <mvittorio29@gmail.com>
2025-06-25 23:54:29 +0200

    Update Config.cs

--- SCP999/Plugin/Config.cs
+++ SCP999/Plugin/Config.cs
@@ -1,10 +1,10 @@
-﻿namespace SCP999;
+namespace SCP999;
 
 public class Config
 {
     public int MinPlayer { get; set; } = 12;
     public bool CassieMsg { get; set; } = true;
     public float AbilityRadius { get; set; } = 5.0f;
-    public string HintMsg { get; set; } = "<color=orange><b>SCP-999</b></color>\\nCura gli umani\\nRallenta gli SCP";
+    public string HintMsg { get; set; } = "<color=orange><b>SCP-999</b></color>\\nAiuta gli umani\\nCura gli umani\\nRallenta gli SCP";
     public float HintDuration { get; set; } = 10.0f;
 }
