f561e85137ef8babbf6e5b063ef1ee371573d22b
Minion6011 <mvittorio29@gmail.com>
2025-06-30 23:07:06 +0200

    Add files via upload

---
+++ CustomZombie/LabApi-DebugCommand/SSSetting/CustomSettingBase.cs
@@ -0,0 +1,23 @@
+﻿using UserSettings.ServerSpecific;
+
+namespace ZombiCustom.SSSetting
+{
+    public abstract class CustomSettingsBase
+    {
+        private static CustomSettingsBase _instance;
+
+        /// <summary>
+        /// Name
+        /// </summary>
+        public abstract string Name { get; }
+
+        /// <summary>
+        /// Quick description of the setting
+        /// </summary>
+        public abstract string Description { get; }
+
+        public abstract void Activate();
+        public abstract void Deactivate();
+        public abstract void ProcessUserInput(ReferenceHub hub, ServerSpecificSettingBase setting);
+    }
+}
