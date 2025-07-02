494d29f40d544b17f69f8db28a3eaa32e82f29b1
Minion6011 <mvittorio29@gmail.com>
2025-07-03 00:58:41 +0200

    Update SpyManager.cs

--- TheSpy/SpyManager.cs
+++ TheSpy/SpyManager.cs
@@ -1,5 +1,7 @@
-﻿using LabApi.Features.Wrappers;
+using LabApi.Features.Console;
+using LabApi.Features.Wrappers;
 using System.Collections.Generic;
+using System.Linq;
 
 
 namespace TheSpy;
@@ -21,6 +23,34 @@ public class SpyManager
         if (SpyPlayers.Contains(player))
         {
             SpyPlayers.Remove(player);
+        }
+    }
+
+    public static void EndRoundCheck() {
+        if (Player.ReadyList.Count() > 1 && SpyPlayers.Count() > 0 && !Round.IsLocked) {
+            int totalPlayers = 0;
+            int totalNTF = 0;
+            int totalChaos = 0;
+            foreach (Player playerList in Player.ReadyList)
+            {
+                if (playerList != null && playerList.IsAlive)
+                {
+                    totalPlayers += 1;
+                    if ((playerList.IsNTF && !SpyManager.SpyPlayers.Contains(playerList)) || playerList.Role == PlayerRoles.RoleTypeId.Scientist || playerList.Role == PlayerRoles.RoleTypeId.FacilityGuard || (playerList.IsChaos && SpyManager.SpyPlayers.Contains(playerList)))
+                    {
+                        totalNTF += 1;
+                    }
+                    else if ((playerList.IsChaos && !SpyManager.SpyPlayers.Contains(playerList)) || playerList.Role == PlayerRoles.RoleTypeId.ClassD || (playerList.IsNTF && SpyManager.SpyPlayers.Contains(playerList)))
+                    {
+                        totalChaos += 1;
+                    }
+                }
+            }
+            // End Round Check
+            if (totalPlayers == totalNTF || totalPlayers == totalChaos)
+            {
+                Round.End();
+            }
         }
     }
 
