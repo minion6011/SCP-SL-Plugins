1e24114a4949e9a7286daba6c1cf1f78d3ccf3aa
Minion6011 <mvittorio29@gmail.com>
2025-07-03 15:38:50 +0200

    Add files via upload

---
+++ SCP3114/Config.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace Heavy3114;
+
+public class Config
+{
+    public int MinPlayer { get; set; } = 25;
+    public string SCP3114Hint { get; set; } = "<color=red>SCP-3114</color>\\nAspetta 45s prima che la porta si apra";
+    public ushort HintDuration { get; set; } = 15;
+    [Description("Tempo dopo il quale la porta di 3114 si aprirà (Stanza di SCP-127)")]
+    public float DoorOpenTime { get; set; } = 45;
+}
---
+++ SCP3114/EventsHandler.cs
@@ -0,0 +1,39 @@
+﻿using LabApi.Events.CustomHandlers;
+using LabApi.Features.Wrappers;
+using MEC;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Heavy3114;
+
+
+public class EventsHandler : CustomEventsHandler
+{
+    static System.Random rnd = new System.Random();
+    public override void OnServerRoundStarted()
+    {
+        if (Server.PlayerCount >= Plugin.Singleton.Config.MinPlayer)
+        {
+            List<Player> players = new List<Player>();
+            foreach (var player in Player.ReadyList)
+            {
+                if (player != null && player.IsSCP)
+                {
+                    players.Add(player);
+                }
+            }
+            if (players.Count > 0)
+            {
+                int r = rnd.Next(players.Count);
+                Player SelectedPlayer = players[r];
+                Door Scp127Door = Door.Get("HCZ_127_LAB");
+                SelectedPlayer.SetRole(PlayerRoles.RoleTypeId.Scp3114);
+                SelectedPlayer.SendHint(Plugin.Singleton.Config.SCP3114Hint, Plugin.Singleton.Config.HintDuration);
+                Scp127Door.IsLocked = true;
+                Scp127Door.IsOpened = false;
+                SelectedPlayer.Position = Scp127Door.Position + new Vector3(0, 0.3f, 0);
+                Timing.CallDelayed(Plugin.Singleton.Config.DoorOpenTime, () => { Scp127Door.IsLocked = false; Scp127Door.IsOpened = true; }); 
+            }
+        }
+    }
+}
