0a003373769bf0d48e9570b931a5ac64fd5558e6
Minion6011 <mvittorio29@gmail.com>
2025-07-05 23:15:12 +0200

    Update Config.cs

--- TheSpy/Config.cs
+++ TheSpy/Config.cs
@@ -4,7 +4,7 @@ public class Config
 {
     public string DamageReason { get; set; } = "Sei stato ucciso da una spia";
     public string SpyHint { get; set; } = "<color=red>Spia</color>\\nTradisci il tuo team";
-    public ushort SpyHintDuration { get; set; } = 15;
+    public ushort SpyHintDuration { get; set; } = 20;
     public int SpyShield { get; set; } = 30;
     public int MinWaveSize { get; set; } = 7;
 }
