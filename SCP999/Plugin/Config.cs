f5d170f6ae20a881bfdaaf5e17c8b7842eff4e44
Minion6011 <mvittorio29@gmail.com>
2025-07-25 15:35:37 +0200

    Update Config.cs

--- SCP999/Plugin/Config.cs
+++ SCP999/Plugin/Config.cs
@@ -1,4 +1,6 @@
-namespace SCP999;
+using System.ComponentModel;
+
+namespace SCP_999;
 
 public class Config
 {
@@ -7,4 +9,13 @@ public class Config
     public float AbilityRadius { get; set; } = 5.0f;
     public string HintMsg { get; set; } = "<color=orange><b>SCP-999</b></color>\\nAiuta gli umani\\nCura gli umani\\nRallenta gli SCP";
     public float HintDuration { get; set; } = 10.0f;
+
+    [Description("Config dell'abilità di SCP-999")]
+    public float HintCooldownDuration { get; set; } = 5;
+    public float KeyAbilityCooldown { get; set; } = 45;
+    public int KeyAbilityHp { get; set; } = 150;
+    public float KeyAbilityRadius { get; set; } = 5.5f;
+    public byte KeyAbilityIntesity { get; set; } = 5;
+    public float KeyAbilityDuration { get; set; } = 25;
+    
 }
